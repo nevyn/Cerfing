59b1804c3bc18fb015f36ea4c91f2d6bc9c0b84b
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2015-06-14 21:18:25 -0700

    Kill EmiNet compatibility
    It just made things too complicated and never worked. Might work on
    removing the whole 'transport' abstraction entirely later.

--- TCAHPEmiConnectionTransport.h
+++
@@ -1,8 +0,0 @@
-#import "TCAHPTransport.h"
-#import <EmiNet/EmiConnection.h>
-
-/** Wraps an EmiConnection and uses it as a transport for a TCAsyncHashProtocol. */
-@interface TCAHPEmiConnectionTransport : TCAHPTransport
-@property(nonatomic,readonly,retain) EmiConnection *connection;
-- (id)initWithConnection:(EmiConnection*)connection delegate:(id<TCAHPTransportDelegate>)delegate;
-@end
